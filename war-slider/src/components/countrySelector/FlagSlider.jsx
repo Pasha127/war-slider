1149178a4658bd3056ee1a6e2246ab4d8e1f6e43
Pasha127 <pasha.account@gmail.com>
2023-03-15 05:32:14 +0100

    animations finished
    need to clean up some styles but core animations are finished

--- war-slider/src/components/countrySelector/FlagSlider.jsx
+++ war-slider/src/components/countrySelector/FlagSlider.jsx
@@ -38,11 +38,19 @@ const FlagSlider = (props) => {
     const [leftArrow, setLeftArrow] = useState(arrowBtn);
     const [vw, setVw] = useState(window.innerWidth);
     const [page, setPage] = useState(1);
+    const [tankKey, setTankKey] = useState(0);
+    const [countryKey, setCountryKey] = useState(2);
+    const [flagKey, setFlagKey] = useState(3);
     const {tankCategory} = props;
     const numberOfTanks = tankData[tankData.findIndex(country => country.name === selectedCountry)].number;
     const listOfTanks = tankData[tankData.findIndex(country =>country.name === selectedCountry)].tanks.join(", ");
     
-    
+    const animate = () => {
+         setTankKey(Math.floor(Math.random() * 100000))
+         setCountryKey(Math.floor(Math.random() * 100000))
+         setFlagKey(Math.floor(Math.random() * 100000))
+    }
+
     useState(() => {
         window.addEventListener("resize", () => {
             setVw(window.innerWidth);
@@ -83,15 +91,15 @@ const FlagSlider = (props) => {
     
     return(<div className="flag-slider">
         <div className="tanks-container">
-            <div className="flag-container">
-                <img src={largeFlagURL + selectedCountry +".webp"} alt="flag" className="large-flag"/>
+            <div className="flag-container" >
+                <img src={largeFlagURL + selectedCountry +".webp"} alt="flag" key={flagKey} className="large-flag"/>
             </div>
             <div className="background">
-            <img src={tankURL + selectedCountry +".webp"} alt="tanks" className="tanks" />
+            <img src={tankURL + selectedCountry +".webp"} alt="tanks" className="tanks" key={tankKey} />
             </div>
             {/* <img src={backgroundURL} className="background"></img> */}
         </div>
-        <div className="country-title">
+        <div className="country-title" key={countryKey}>
             <p>{selectedCountry}</p>
         </div>
         <div className="slider-bottom">
@@ -110,6 +118,7 @@ const FlagSlider = (props) => {
                 </div>
             {vw < 800 ? <Swiper {...sliderSettings}
             onSlideChange={(swiper) => {
+                animate();
                 setSelectedCountry(tankData[swiper.realIndex]?.name);
                 setActiveSlide(swiper.realIndex);}}
                 >
@@ -136,7 +145,10 @@ const FlagSlider = (props) => {
                 <div className={`first-row-${page}`}>
                 {tankData.slice(1,6).map((country,index)=>{
                    return (<div key={"row 1 flag"+index} className="flag-item"
-                    onClick={()=> setSelectedCountry(country.name)}
+                    onClick={()=> {
+                        setSelectedCountry(country.name);
+                        animate();
+                    }}
                    >
                         <img src={smallFlagURL+country.name+"_small.png"} alt="flag"/>
                         <div className="flag-subtitle">{country.name}</div>
@@ -146,7 +158,10 @@ const FlagSlider = (props) => {
                 <div className={`second-row-${page}`}>
                 {tankData.slice(7,11).map((country,index)=>{
                    return (<div key={"row 2 flag"+index} className="flag-item"
-                   onClick={()=> setSelectedCountry(country.name)}
+                   onClick={()=> {
+                    setSelectedCountry(country.name);
+                    animate();
+                }}
                    >
                     
                      <img src={smallFlagURL+country.name+"_small.png"} alt="flag"/>
